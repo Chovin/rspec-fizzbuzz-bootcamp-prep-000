d7739dc2a18212519b833edf1881a9e17b04b33a
Chovin Carlson <Chovin@users.noreply.github.com>
2018-08-31 01:01:33 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,9 @@
 def fizzbuzz(int)
-  ("Fizz" if int % 3 == 0) + ("Buzz" if int % 5 == 0)
+  if int % 15 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
+  elsif int % 5 == 0
+    "Buzz"
+  end
 end
